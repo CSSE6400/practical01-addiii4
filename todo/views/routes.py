918b3986ef60baea17acfd8d7adcb84ee2954a1d
addiii4 <adityacheema4@gmail.com>
2023-03-06 02:04:44 +1000

    fixed

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,30 +1,44 @@
-@baseUrl = http://localhost:6400
+from flask import Blueprint, jsonify
+ 
+api = Blueprint('api', __name__, url_prefix='/api/v1') 
 
-### Health
-GET {{baseUrl}}/api/v1/health
-
-### List All Todos
-GET {{baseUrl}}/api/v1/todos
-
-### Get a specific Todo
-GET {{baseUrl}}/api/v1/todos/1
-
-### Create a Todo
-POST {{baseUrl}}/api/v1/todos
-Content-Type: application/json
-
-{
-    "title": "An example Todo",
-    "description": "This is an example todo",
+TEST_ITEM = {
+    "id": 1,
+    "title": "Watch CSSE6400 Lecture",
+    "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+    "completed": True,
+    "deadline_at": "2023-02-27T00:00:00",
+    "created_at": "2023-02-20T00:00:00",
+    "updated_at": "2023-02-20T00:00:00"
 }
+ 
+@api.route('/health') 
+def health():
+    """Return a status of 'ok' if the server is running and listening to request"""
+    return jsonify({"status": "ok"})
 
-### Update a Todo
-PUT {{baseUrl}}/api/v1/todos/1
-Content-Type: application/json
 
-{
-    "title": "updated title",
-}
+@api.route('/todos', methods=['GET'])
+def get_todos():
+    """Return the list of todo items"""
+    return jsonify([TEST_ITEM])
 
-### Delete a Todo
-DELETE {{baseUrl}}/api/v1/todos/1
+@api.route('/todos/<int:todo_id>', methods=['GET'])
+def get_todo(todo_id):
+    """Return the details of a todo item"""
+    return jsonify(TEST_ITEM)
+
+@api.route('/todos', methods=['POST'])
+def create_todo():
+    """Create a new todo item and return the created item"""
+    return jsonify(TEST_ITEM), 201
+
+@api.route('/todos/<int:todo_id>', methods=['PUT'])
+def update_todo(todo_id):
+    """Update a todo item and return the updated item"""
+    return jsonify(TEST_ITEM)
+
+@api.route('/todos/<int:todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    """Delete a todo item and return the deleted item"""
+    return jsonify(TEST_ITEM)
